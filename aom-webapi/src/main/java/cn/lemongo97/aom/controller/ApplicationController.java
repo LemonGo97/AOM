eb3cc0041085787f766af98778795eb3786511e6
PC\LemonGo97 <lemongo97@outlook.com>
2021-11-01 01:56:07 +0800

    API 更改为 Restful 风格，增加两个未完成的 TODO 接口

--- aom-webapi/src/main/java/cn/lemongo97/aom/controller/ApplicationController.java
+++ aom-webapi/src/main/java/cn/lemongo97/aom/controller/ApplicationController.java
@@ -7,7 +7,10 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 /**
  * @author LemonGo97
@@ -19,8 +22,20 @@ public class ApplicationController {
     @Autowired
     private ApplicationService applicationService;
 
-    @GetMapping("/applications/version")
-    public IPage<ApplicationVersionPO> listVersion(String type, Page<ApplicationVersionPO> pageInfo){
+    @GetMapping("/applications/{type}/info")
+    public String getInfo(@PathVariable(value = "type") String type){
+        // todo 获取应用概述
+        return null;
+    }
+
+    @GetMapping("/applications/{type}/changeLog")
+    public String changeLog(@PathVariable(value = "type") String type){
+        // todo 获取应用更新日志
+        return null;
+    }
+
+    @GetMapping("/applications/{type}/version")
+    public IPage<ApplicationVersionPO> listVersion(@PathVariable(value = "type") String type, Page<ApplicationVersionPO> pageInfo){
         return applicationService.listVersion(type,pageInfo);
     }
 
